d6e865d4eedb5d707cc46fb97f1fb5967bd87b7c
Eric Theuri <tericmail001@gmail.com>
2025-06-19 23:18:05 +0300

    Added the change status function

--- utils.py
+++ utils.py
@@ -34,13 +34,13 @@ def add_tasks():
                         continue
         task_due_date = due_date_checker()
         task_status = "Incomplete"
-        task = Task(task_id,task_title,task_description,task_due_date,task_status).__dict__
+        task = Task(task_id,task_title,task_description,str(task_due_date),task_status).__dict__
         #File section
         filename = "tasks.json"
         if os.path.exists(filename) and os.path.getsize(filename) > 0:
             with open(filename, "r") as tasks_file:
                 try:
-                    task_list = json.load(file)
+                    task_list = json.load(tasks_file)
                 except json.JSONDecodeError:
                     print("Corrupted JSON file.")
                     task_list = []
@@ -49,7 +49,9 @@ def add_tasks():
         
         task_list.append(task)
         with open(filename, "w") as file:
-            json.dump(task_list,tasks_file, indent=4)
+            json.dump(task_list,file, indent=4)
+        
+        file.close()
                 
 
         
@@ -58,6 +60,7 @@ def add_tasks():
 
 ###Function to show all tasks
 def load_tasks():
+    
     print("Here are all the tasks")
     time.sleep(0.3)
     with open("tasks.json", "r") as tasks_file:
@@ -68,6 +71,7 @@ def load_tasks():
         print(f"Task Id: {task['id']}\nTitle: {task['title']}\nDescription: {task['description']}\nDue date: {task['due_date']}\nStatus: {task['completed']}\n")
     
     return task_list
+
 #Function to delete tasks by id
 def delete_task():
     id_list = None
@@ -82,10 +86,54 @@ def delete_task():
     print("Task deleted")
 #Function to edit the status to either 'complete' or 'incomplete'
 def edit_status():
-    print("To change status, I will preview all the tasks and you choose which one to modify")
-    status_modified = int(input(f"{load_tasks()}\n***** Choose the task using it's id. \n > "))
+    
+    tasks_id = []
+    load_tasks()
+    print("To change status, I will preview all the tasks and you choose which one to modify...\n")
+    status_modified = int(input(f"***** Choose the task using it's id. \n > "))
+    filename = "tasks.json"
+    with open(filename, "r") as tasks_list:
+        tasks = json.load(tasks_list)
+    for task in tasks:
+        print(f"Task id: {task['id']} : {task['title']} : {task['completed']}")
+        tasks_id.append(task['id'])
+
+    if status_modified in tasks_id:
+        for task in tasks:
+            if task['id'] == status_modified:
+                if task['completed'] == "Incomplete":
+                    new_status = "Completed" 
+                elif task['completed'] == "Completed":
+                    new_status = "Incomplete"
+                else:
+                    new_status = "Incomplete"
+                
+                change_status = input(f"Change status to {new_status}? y or n \n> ").lower()
+
+                if change_status == "y":
+                    task['completed'] = new_status
+                    print(f"Task status has successfully been changed to {new_status}")
+                    with open(filename, "w") as tasks_list:
+                        json.dump(tasks, tasks_list, indent=4)
+                elif change_status == "n":
+                    print(f"The status remains {task['completed']}")
+                else:
+                    print("Please insert y or n")
+
+    else:
+        print(f"Sorry! The task by the id {status_modified} does not exist) :(")
+
+    
+    
+
+    
+       
+
 #Function to change the status of a task to either 
 def status_marker():
     print("The status has been marked")
 ###Id generation and input validation logic is handled here
 ##Write tests for these functions using unittest in test_utils.py
+
+if __name__ == "__main__":
+    edit_status()
